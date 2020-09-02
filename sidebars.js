17a02be5e0bb3d8c3cf6a0111cc5a79749bf1ff0
Nabeel Shahzad <nshahzad@gmail.com>
2020-09-01 22:25:04 -0400

    Refactor sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
-  mainSidebar: {
-    gettingStarted: {
+  docs: [
+    {
       type: 'category',
       label: 'Getting Started',
       collapsed: false,
@@ -8,40 +8,62 @@ module.exports = {
         'getting-started/intro',
         'getting-started/requirements',
         'getting-started/download',
-      ],
+      ]
     },
-    'Installation': [
-      'installation/uploading',
-      'installation/vhost',
-      'installation/installation',
-      'installation/cron',
-      'installation/importing',
-      'installation/upgrading',
-    ],
-    'Configuration': [
-      'config/files',
-      'config/optimizing',
-    ],
-    'Themes and Layouts': [
-      'customize/layouts',
-      'customize/theming',
-      'customize/building-assets',
-    ],
-    'Developers': [
-      'developers/environment',
-      'developers/addons',
-      'developers/awards',
-    ],
-    'API': [
-      'api/overview',
-      'api/auth',
-      'api/apis',
-    ],
-    'ACARS': [
-      'acars/install-server',
-    ],
-    'Help': [
-      'help/getting-help',
-    ]
-  },
+    {
+      type: 'category',
+      label: 'Installation',
+      items: [
+        'installation/uploading',
+        'installation/vhost',
+        'installation/installation',
+        'installation/cron',
+        'installation/importing',
+        'installation/upgrading',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Configuration',
+      items: [
+        'config/files',
+        'config/optimizing',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Themes and Layouts',
+      items: [
+        'customize/layouts',
+        'customize/theming',
+        'customize/building-assets',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Developers',
+      items: [
+        'developers/environment',
+        'developers/addons',
+        'developers/awards',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'API',
+      items: [
+        'api/overview',
+        'api/auth',
+        'api/apis',
+      ]
+    },
+    {
+      type: 'category',
+      label: 'ACARS',
+      items: [
+        'acars/install-server',
+      ]
+    },
+    'help/getting-help',
+  ]
 };
